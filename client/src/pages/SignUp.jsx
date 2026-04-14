fab3d3f613454610ec64e57df940d52370845e89
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-04-14 20:03:42 +0530

    added login css

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,9 +3,15 @@ import UserSignUp from "../components/auth/UserSignUp";
 const Login = ({ onSignup }) => {
   return (
     <div className="login">
-      <div className="left"></div>
+      <div className="left">
+        <h1>Hello 👋</h1>
+        <p>Sign up to continue to your dashboard</p>
+      </div>
       <div className="right">
-        <UserSignUp onSignup={onSignup} />
+        <div className="right-box">
+          {" "}
+          <UserSignUp onSignup={onSignup} />
+        </div>
       </div>
     </div>
   );
